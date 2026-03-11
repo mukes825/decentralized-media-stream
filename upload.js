d5093a83bca5172ebb8d2b892741fc902244c043
mukesh kumar <vinaykurmi825@gmail.com>
2026-03-11 10:22:34 +0530

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+function uploadMedia(title,type){
+
+ const media = JSON.parse(fs.readFileSync("media.json"));
+
+ media.files.push({title,type});
+
+ fs.writeFileSync("media.json",JSON.stringify(media,null,2));
+
+ console.log("Media uploaded");
+}
+
+uploadMedia("Creator Demo Video","video");
